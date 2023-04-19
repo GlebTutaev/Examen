ae26c7efe1fb056fe3512ff5205df3bdd0ffebfb
Глеб Тутаев <aka-laka-006@mail.ru>
2023-04-19 13:49:50 +0500

    CommitDone

---
+++ Examen/MVVM/View/discoveryView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examen.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для discoveryView.xaml
+    /// </summary>
+    public partial class discoveryView : UserControl
+    {
+        public discoveryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Examen/MVVM/ViewModel/MainViewModel.cs
+++ Examen/MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Examen.Core;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +9,57 @@ using System.Threading.Tasks;
 
 namespace Examen.MVVM.ViewModel
 {
-    class MainViewModel: ObservableObject
+    class MainViewModel : Design
     {
+        public RelayCommand ViewCommand { get; set; }
+
+        public RelayCommand DiscoveryviewCommand { get; set; }
+
+        public RelayCommand OrgCommand { get; set; }
+        
+
+
+        public ViewModel VM { get; set; }
+        public DiscoveryViewModel DVM { get; set; }
+        public OrgModel OVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertryChanged();
+
+            }
+        }
+
+
 
         public MainViewModel() 
         {
+            VM = new ViewModel();
+            DVM = new DiscoveryViewModel();
+            OVM = new OrgModel();
+            CurrentView = VM;
 
+            ViewCommand = new RelayCommand(() =>
+            {
+                CurrentView = VM;
+            });
+
+            DiscoveryviewCommand = new RelayCommand(() =>
+            {
+                CurrentView = DVM;
+            });
+
+            OrgCommand = new RelayCommand(() =>
+            {
+                CurrentView = OVM;
+            });
         }
     }
 }
+ 
